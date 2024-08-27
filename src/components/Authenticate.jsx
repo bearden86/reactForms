d3809abbb5337daff763da02abdded039d8e767d
optik1 <bearden86@gmail.com>
2024-08-27 09:18:01 -0400

    remove boilerplate, create signup and authenticate components

---
+++ src/components/Authenticate.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+
+export default function Authenticate() {
+    return <h2>Please Authenticate</h2>
+}
---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,6 @@
+//create a signup form component and set as default export
+import React, { useState } from 'react';
+
+export default function SignUpForm() {
+    return <h2>Sign Up!</h2>
+}
